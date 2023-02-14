65cd0a40004ef48e00404898de856953187896d0
STUDENT Darragh Quinn <Darragh.Quinn@students.ittralee.ie>
2023-02-14 16:02:28 +0000

    Redid grappling and added it to movement script, dual grapples working, dual pull working
    Signed-off-by: STUDENT Darragh Quinn <Darragh.Quinn@students.ittralee.ie>

--- Assets/Grapple.cs
+++ Assets/Grapple.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 public class Grapple : MonoBehaviour
 {
-    private LineRenderer LR;
+
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
 
     public Transform RWebTip, LWebTip, player;
 
-    private float maxDistance = 100f;
+
     private SpringJoint leftWeb, rightWeb;
 
     public KeyCode pullKey = KeyCode.F;
@@ -25,97 +25,20 @@ public class Grapple : MonoBehaviour
     }
 
 
-    private void Awake()
-    {
-        LR = GetComponent<LineRenderer>();
-    }
-
-    private void Update()
-    {
 
 
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            rightWeb = StartGrapple();
-        }
-        else if (Input.GetKeyUp(KeyCode.E))
-        {
-            StopGrapple(rightWeb);
-        }
-
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            leftWeb = StartGrapple();
-        }
-        else if (Input.GetKeyUp(KeyCode.Q))
-        {
-            StopGrapple(leftWeb);
-        }
-
-        if (Input.GetKey(KeyCode.R))
-        {
-            pullWeb(ref rightWeb);
-        }
-    }
-
-    private void pullWeb(ref SpringJoint rightWeb)
-    {
-        rightWeb.maxDistance *= 0.7f;
-        rightWeb.spring *= 1.01f;
-        
-    }
-
-    private void LateUpdate()
-    {
-        DrawRope(leftWeb);
-        DrawRope(rightWeb);
-
-    }
-
-     SpringJoint StartGrapple()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance, whatIsGrappleable))
-        {
-            
-            //grapplePoint = hit.point;
-            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 20f, Color.blue, 3);
-            SpringJoint Web = player.gameObject.AddComponent<SpringJoint>();
-            Web.autoConfigureConnectedAnchor = false;
-            Web.connectedAnchor = hit.point;
 
 
-            Web.maxDistance = distanceFromPoint * 0.8f;
-            Web.minDistance = distanceFromPoint * 0.25f;
 
-            Web.spring = 2.5f;
-            Web.damper = 7f;
-            Web.massScale = 4.5f;
 
-            LR.positionCount = 2;
-            //currentGrapplePosition = WebTip.position;
-            return Web;
-        }
-        return null;
-    }
+
+
 
     private Vector3 currentGrapplePosition;
 
-    void DrawRope(SpringJoint Web)
-    {
-        if (!Web) return;
 
-        RWebTip.position = Vector3.Lerp(RWebTip.position, Web.connectedAnchor, Time.deltaTime * 8f);
 
-        LR.SetPosition(0, transform.position);
-        LR.SetPosition(1, Web.connectedAnchor);
-    }
 
-    void StopGrapple(SpringJoint Web)
-    {
-        LR.positionCount = 0;
-        Destroy(Web);
-    }
     public bool IsGrappling(SpringJoint Web)
     {
         return Web != null;
--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,8 +5,12 @@ using TMPro;
 
 public class Movement : MonoBehaviour
 {
+    private float maxDistance = 100f;
+    public Transform LeftShooterTip, RightShooterTip;
     [Header("Movement")]
     public float moveSpeed;
+
+    private LineRenderer LRLeft, LRRight;
 
     public float groundDrag;
 
@@ -38,6 +42,7 @@ public class Movement : MonoBehaviour
     Vector3 extragravity;
 
     Rigidbody rb;
+    private SpringJoint rightWeb, leftWeb;
 
     private void Start()
     {
@@ -45,6 +50,9 @@ public class Movement : MonoBehaviour
         rb.freezeRotation = true;
 
         readyToJump = true;
+
+        LRLeft =LeftShooterTip.parent.GetComponent<LineRenderer>();
+        LRRight = RightShooterTip.parent.GetComponent<LineRenderer>();
     }
 
     private void Update()
@@ -62,8 +70,43 @@ public class Movement : MonoBehaviour
             rb.drag = groundDrag;
         else
             rb.drag = 0;
-            rb.AddForce(Vector3.down * 2, ForceMode.Acceleration); 
-    }
+            rb.AddForce(Vector3.down * 2, ForceMode.Acceleration);
+
+   
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            rightWeb = StartGrapple(transform, LRRight);
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            StopGrapple(rightWeb, LRRight);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            leftWeb = StartGrapple(transform, LRLeft);
+        }
+        else if (Input.GetKeyUp(KeyCode.Q))
+        {
+            StopGrapple(leftWeb, LRLeft);
+        }
+
+        if (Input.GetKey(KeyCode.G))
+        {
+            pullWeb(ref rightWeb);
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            pullWeb(ref leftWeb);
+        }
+
+
+        DrawRope(leftWeb, LRLeft);
+        DrawRope(rightWeb, LRRight);
+
+        
+}
 
     private void FixedUpdate()
     {
@@ -147,4 +190,58 @@ public class Movement : MonoBehaviour
         if (!grounded)
             rb.AddForce(Vector3.forward * 5, ForceMode.Impulse);
     }
+
+
+    SpringJoint StartGrapple(Transform shooter, LineRenderer LR)
+    {
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 20f, Color.blue, 3);
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, maxDistance))
+        {
+
+            //grapplePoint = hit.point;
+
+            SpringJoint Web = shooter.gameObject.AddComponent<SpringJoint>();
+            Web.autoConfigureConnectedAnchor = false;
+            Web.connectedAnchor = hit.point;
+
+
+            Web.maxDistance = hit.distance * 0.8f;
+            Web.minDistance = hit.distance * 0.25f;
+
+            Web.spring = 2.5f;
+            Web.damper = 7f;
+            Web.massScale = 4.5f;
+
+            LR.positionCount = 2;
+
+            //currentGrapplePosition = WebTip.position;
+            return Web;
+        }
+        return null;
+    }
+
+
+
+    private void pullWeb(ref SpringJoint currentWeb)
+    {
+        currentWeb.maxDistance *= 0.7f;
+        currentWeb.spring *= 1.01f;
+
+    }
+    void StopGrapple(SpringJoint Web, LineRenderer LR)
+    {
+
+        LR.positionCount = 0;
+        Destroy(Web);
+    }
+
+
+    void DrawRope(SpringJoint Web,LineRenderer LR)
+    {
+        if (!Web) return;
+        LR.SetPosition(0, LR.transform.position);
+        LR.SetPosition(1, Web.connectedAnchor);
+    }
+
 }
